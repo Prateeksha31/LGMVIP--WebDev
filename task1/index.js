3e6788fd5b78479af026938c7d4869e7170bdc10
Prateeksha31 <67828785+Prateeksha31@users.noreply.github.com>
2021-10-29 18:21:23 +0530

    Add files via upload

---
+++ task1/index.js
@@ -0,0 +1,33 @@
+$("#video_projects .owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    responsive : {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000 : {
+            items: 3
+        }
+    }
+ });
+ $("#LIZ .owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    responsive : {
+        0: {
+            items: 3
+        },
+        600: {
+            items: 5
+        },
+        1000 : {
+            items: 7
+        }
+    }
+ });
+ 
